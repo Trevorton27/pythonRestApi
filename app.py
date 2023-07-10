725cc1910f200203554a2c00b4eeea2685cc754e
Trevor Mearns <spiral272@gmail.com>
2023-07-10 17:04:47 +0900

    created docker image and file. updated api points with comments

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 stores = [{"name": "My Store", "items": [{"name": "Chair", "price": 15.99}]}]
 
-
+#returns list of stores
 @app.get("/store")
 def get_stores():
     return {"stores": stores}
 
-
+#creates a store
 @app.post("/store")
 def create_store():
     request_data = request.get_json()
@@ -17,7 +17,7 @@ def create_store():
     stores.append(new_store)
     return new_store, 201
 
-
+#creates item in specific store
 @app.post("/store/<string:name>/item")
 def create_item(name):
     request_data = request.get_json()
@@ -28,7 +28,7 @@ def create_item(name):
             return new_item, 201
     return {"message": "Store not found"}, 404
 
-
+#returns name of store
 @app.get("/store/<string:name>")
 def get_store(name):
     for store in stores:
@@ -36,7 +36,7 @@ def get_store(name):
             return store
     return {"message": "Store not found"}, 404
 
-
+#returns items in store via store name
 @app.get("/store/<string:name>/item")
 def get_item_in_store(name):
     for store in stores:

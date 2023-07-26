0e26b1e877141e9984d7f26222b342f6abac5868
Trevor Mearns <spiral272@gmail.com>
2023-07-26 21:26:45 +0900

    made app variable global in app.py

--- app.py
+++ app.py
@@ -11,9 +11,10 @@ from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
 
+app = Flask(__name__)
 
 def create_app(db_url=None):
-    app = Flask(__name__)
+  
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
